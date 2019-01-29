2e018225d3798840867e1b27de994e1a8d8ad89c
hemangandhi <hemangandhi@gmail.com>
2019-01-28 20:16:00 -0500

    no geckodriver log

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from getpass import getpass
 
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
 
 class SakaiBrowser:
     def __init__(self):
